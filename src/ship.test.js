a2ee69cb76bfc48ef34255c7382169bc4887b2e8
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-08-22 21:31:51 +0530

    refactor(ship class)

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,31 +1,28 @@
 import { Ship } from "./ship.js";
 
-describe("Carrier", () => {
-  test("Ship is sunk", () => {
+describe("isSunk", () => {
+  test("Carrier: Ship is sunk", () => {
     const carrier = new Ship(5);
     for (let i = 0; i < 5; i++) {
       carrier.hit();
     }
     expect(carrier.isSunk()).toBe(true);
   });
-  test("Ship is not sunk: 4 hits", () => {
+  test("Carrier: Ship is not sunk: 4 hits", () => {
     const carrier = new Ship(5);
     for (let i = 0; i < 4; i++) {
       carrier.hit();
     }
     expect(carrier.isSunk()).toBe(false);
   });
-});
-
-describe("Patrol boat", () => {
-  test("Ship is sunk", () => {
+  test("Patrol boat: Ship is sunk", () => {
     const patrolBoat = new Ship(2);
     for (let i = 0; i < 2; i++) {
       patrolBoat.hit();
     }
     expect(patrolBoat.isSunk()).toBe(true);
   });
-  test("Ship is not sunk: 1 hit", () => {
+  test("Patrol boat: Ship is not sunk: 1 hit", () => {
     const patrolBoat = new Ship(2);
     patrolBoat.hit();
     expect(patrolBoat.isSunk()).toBe(false);
