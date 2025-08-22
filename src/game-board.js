b0734a63e1bbc802ec53f7e13b03906265dfa930
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-08-22 23:36:36 +0530

    feat(GameBoard class): Add addShip class
    - Add addShip class

--- src/game-board.js
+++ src/game-board.js
@@ -1,3 +1,5 @@
+import { Ship } from "./ship.js";
+
 export class GameBoard {
   constructor(size) {
     this.size = size;
@@ -8,11 +10,28 @@ export class GameBoard {
         this.board[i].push("empty");
       }
     }
-    console.log(this.board);
+  }
+  addShip(length, startCoordinate, direction) {
+    let x = startCoordinate[0];
+    let y = startCoordinate[1];
+    this.board[x][y] = new Ship(length);
+    if (direction === "horizontal") {
+      for (let i = 0; i < length; i++) {
+        this.board[x + 1][y] = this.board[x][y];
+        x++;
+      }
+    } else if (direction === "vertical") {
+      for (let i = 0; i < length; i++) {
+        this.board[x][y + 1] = this.board[x][y];
+        y++;
+      }
+    }
   }
   receiveAttack(x, y) {
     if (this.board[x][y] === "empty") {
       this.board[x][y] = "miss";
     }
+    // if (typeof this.board[x][y] === "object") {
+    // }
   }
 }
--- src/game-board.test.js
+++ src/game-board.test.js
@@ -1,9 +1,30 @@
 import { GameBoard } from "./game-board.js";
+let gameBoardOne;
+beforeEach(() => {
+  gameBoardOne = new GameBoard(10);
+});
+
+describe("addShip", () => {
+  test("Add one ship", () => {
+    gameBoardOne.addShip(3, [2, 3], "horizontal");
+    expect(typeof gameBoardOne.board[2][3]).toBe('object');
+    expect(gameBoardOne.board[3][3]).toBe(gameBoardOne.board[2][3]);
+    expect(gameBoardOne.board[4][3]).toBe(gameBoardOne.board[2][3]);
+  });
+  // test('Try to overlap ships', () => {
+
+  // });
+  // test('Try to add more than maximum number of ships', () => {
+
+  // })
+});
 
 describe("receiveAttack", () => {
-  const gameBoardOne = new GameBoard(10);
   test("Missed attack", () => {
     gameBoardOne.receiveAttack(1, 3);
     expect(gameBoardOne.board[1][3]).toBe("miss");
   });
+  // test('Hit attack', () => {
+
+  // });
 });
